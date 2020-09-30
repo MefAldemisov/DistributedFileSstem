c4ddfa09b4ef415c8bb220585298316199517dd9
MefAldemisov <alisha-omg@yandex.ru>
2020-09-30 20:19:43 +0300

    Updated file creation icin and the order of icns

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,17 +2,17 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
         <div>
-            <icon type="copy" width="2rem"></icon>
-            <icon type="download" width="2rem"></icon>
-            <icon type="info" width="2rem"></icon>
-            <icon type="mkdir" width="2rem"></icon>
-            <icon type="move" width="2rem"></icon>
             <icon type="refresh" width="2rem"></icon>
+            <icon type="copy" width="2rem"></icon>
+            <icon type="move" width="2rem"></icon>
+            <icon type="mkdir" width="2rem"></icon>
             <icon type="rm_dir" width="2rem"></icon>
-            <icon type="rm_file" width="2rem"></icon>
-            <icon type="rm_rf" width="2rem"></icon>
             <icon type="touch" width="2rem"></icon>
+            <icon type="rm_file" width="2rem"></icon>
+            <icon type="download" width="2rem"></icon>
             <icon type="upload" width="2rem"></icon>
+            <icon type="info" width="2rem"></icon>
+            <icon type="rm_rf" width="2rem"></icon>
         </div>
         <table>
             <thead>
@@ -21,7 +21,12 @@
                 <th>Last update</th>
             </thead>
             <tbody>
-                <tr v-for="file in files" :key="file.name">
+                <tr
+                    :class="{ active: file.index == active }"
+                    v-for="file in files"
+                    :key="file.index"
+                    @click="setActive(file.index)"
+                >
                     <td>{{ file.name }}</td>
                     <td>{{ file.size }}</td>
                     <td>{{ file.upd }}</td>
@@ -42,10 +47,58 @@ export default {
     data() {
         return {
             files: [
-                { name: "helloworld.c", size: "256", upd: "23.05.19" },
-                { name: "helloworld.cpp", size: "184", upd: "23.05.20" },
+                {
+                    index: 3,
+                    name: "./dist",
+                    size: "184",
+                    upd: "23.05.20",
+                    data: [],
+                },
+                {
+                    index: -1,
+                    name: "dir",
+                    size: "1024",
+                    data: [
+                        {
+                            index: 0,
+                            name: "helloworld.c",
+                            size: "256",
+                            upd: "23.05.19",
+                        },
+                        {
+                            index: 1,
+                            name: "helloworld.cpp",
+                            size: "184",
+                            upd: "23.05.20",
+                        },
+                    ],
+                },
+                {
+                    index: 0,
+                    name: "helloworld.c",
+                    size: "256",
+                    upd: "23.05.19",
+                },
+                {
+                    index: 1,
+                    name: "helloworld.cpp",
+                    size: "184",
+                    upd: "23.05.20",
+                },
+                {
+                    index: 2,
+                    name: "aaa.txt",
+                    size: "256",
+                    upd: "23.05.19",
+                },
             ],
+            active: 0,
         };
+    },
+    methods: {
+        setActive(index) {
+            this.active = index;
+        },
     },
 };
 </script>
@@ -64,6 +117,10 @@ table {
     margin: auto;
 }
 tr:hover {
+    background-color: bisque;
+}
+.active {
+    font-weight: 800;
     background-color: bisque;
 }
 </style>
