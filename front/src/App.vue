9745e8a33a78e1d802c1e6fc6ba909d637a89601
MefAldemisov <alisha-omg@yandex.ru>
2020-09-30 18:59:05 +0300

    Icons for all clients interfaces added

---
+++ front/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <div>
+            <icon type="copy" width="2rem"></icon>
+            <icon type="download" width="2rem"></icon>
+            <icon type="info" width="2rem"></icon>
+            <icon type="mkdir" width="2rem"></icon>
+            <icon type="move" width="2rem"></icon>
+            <icon type="refresh" width="2rem"></icon>
+            <icon type="rm_dir" width="2rem"></icon>
+            <icon type="rm_file" width="2rem"></icon>
+            <icon type="rm_rf" width="2rem"></icon>
+            <icon type="touch" width="2rem"></icon>
+            <icon type="upload" width="2rem"></icon>
+        </div>
+        <table>
+            <thead>
+                <th>Filename</th>
+                <th>Size</th>
+                <th>Last update</th>
+            </thead>
+            <tbody>
+                <tr v-for="file in files" :key="file.name">
+                    <td>{{ file.name }}</td>
+                    <td>{{ file.size }}</td>
+                    <td>{{ file.upd }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import Icon from "./components/Icon.vue";
+
+export default {
+    name: "App",
+    components: {
+        Icon,
+    },
+    data() {
+        return {
+            files: [
+                { name: "helloworld.c", size: "256", upd: "23.05.19" },
+                { name: "helloworld.cpp", size: "184", upd: "23.05.20" },
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+table {
+    width: 60%;
+    margin: auto;
+}
+tr:hover {
+    background-color: bisque;
+}
+</style>
---
+++ front/src/components/Icon.vue
@@ -0,0 +1,63 @@
+<script>
+import IconCopy from "./icons/IconCopy.vue";
+import IconDownload from "./icons/IconDownload.vue";
+import IconInfo from "./icons/IconInfo.vue";
+import IconMkDir from "./icons/IconMkDir.vue";
+import IconMove from "./icons/IconMove.vue";
+import IconRefresh from "./icons/IconRefresh.vue";
+import IconRmDir from "./icons/IconRmDir.vue";
+import IconRmFile from "./icons/IconRmFile.vue";
+import IconRmRf from "./icons/IconRmRf.vue";
+import IconTouch from "./icons/IconTouch.vue";
+import IconUpload from "./icons/IconUpload.vue";
+
+export default {
+    name: "Icon",
+    props: {
+        type: {
+            default: "info",
+        },
+        width: {
+            default: "1.5rem",
+        },
+    },
+    components: {
+        IconCopy,
+        IconDownload,
+        IconInfo,
+        IconMkDir,
+        IconMove,
+        IconRefresh,
+        IconRmDir,
+        IconRmFile,
+        IconRmRf,
+        IconTouch,
+        IconUpload,
+    },
+};
+</script>
+<template>
+    <div class="inline hoverable" :title="type">
+        <icon-copy :width="width" v-if="type == 'copy'"></icon-copy>
+        <icon-download :width="width" v-if="type == 'download'"></icon-download>
+        <icon-info :width="width" v-if="type == 'info'"></icon-info>
+        <icon-mk-dir :width="width" v-if="type == 'mkdir'"></icon-mk-dir>
+        <icon-move :width="width" v-if="type == 'move'"></icon-move>
+        <icon-refresh :width="width" v-if="type == 'refresh'"></icon-refresh>
+        <icon-rm-dir :width="width" v-if="type == 'rm_dir'"></icon-rm-dir>
+        <icon-rm-file :width="width" v-if="type == 'rm_file'"></icon-rm-file>
+        <icon-rm-rf :width="width" v-if="type == 'rm_rf'"></icon-rm-rf>
+        <icon-touch :width="width" v-if="type == 'touch'"></icon-touch>
+        <icon-upload :width="width" v-if="type == 'upload'"></icon-upload>
+    </div>
+</template>
+<style scoped>
+.inline {
+    display: inline-block;
+    margin: 10px;
+}
+.hoverable:hover {
+    transform: scale(1.5);
+}
+</style>
+
